690f13ea254721b95b4f7b6da333e99530554a12
Obcoder <obcoder@gmail.com>
2023-11-16 08:31:38 +0300

    +vers

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -24,6 +24,12 @@ defineProps({
         <article>
             <slot />
         </article>
+        <article>
+            Май ли уже расцвел над городом,
+            плачет ли, как побитый, хмуренький декабрик, -
+            весь год эта пухлая морда
+            маячит в дымах фабрик.
+        </article>
     </main>
 
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
