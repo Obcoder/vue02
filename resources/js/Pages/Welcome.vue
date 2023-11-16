fbf270bb9a57b17725f8cf053a40c481c4185794
Obcoder <obcoder@gmail.com>
2023-11-16 03:31:20 +0300

    fish divs

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -46,6 +46,12 @@ defineProps({
                     <p>Maybe we can live without...</p>
                     <p>Look. If you think this is...</p>
                 </div>
+                <div class="grid grid-cols-4 gap-1 bg-red-100">
+                    <div>basa</div>
+                    <div>flounder</div>
+                    <div>hake</div>
+                    <div>herring</div>
+                </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
