09fd2f27e1fd1cd384f8b41b565bd72c63399dec
Obcoder <obcoder@gmail.com>
2023-11-17 08:21:52 +0300

    debag app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,13 +11,13 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    // resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    resolve: (name) => {
-        const page = require(`./Pages/${name}.vue`).default()
-        page.layout = page.layout || MainLayout
-
-        return page
-    },
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    // resolve: (name) => {
+    //     const page = require(`./Pages/${name}.vue`).default()
+    //     page.layout = page.layout || MainLayout
+    //
+    //     return page
+    // },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
