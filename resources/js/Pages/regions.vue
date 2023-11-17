581905500f1860b515770a63f2929d56d7cc6caa
Obcoder <obcoder@gmail.com>
2023-11-17 09:27:23 +0300

    list rendering

--- resources/js/Pages/regions.vue
+++ resources/js/Pages/regions.vue
@@ -1,13 +1,20 @@
 <template>
     <MainLayout>
-        <div>
-            Адыгея
+        <h1>
+            Регионы
+        </h1>
+        <div v-for="region in regions" :key="region">
+            {{region}}
         </div>
     </MainLayout>
 </template>
 
 <script>
 import MainLayout from "@/Layouts/MainLayout.vue";
+
+defineProps({
+    regions: Array,
+});
 export default {
     name: "regions",
     components: {MainLayout}
